1578af4f4e388482f9263c2780ee08d643ad5511
Natalia Waś <nataliawas@wp.pl>
2019-03-23 09:52:19 +0100

    chrome extension redux dev tool added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case INCREMENT_COUNTER:
+            return state + 1
+        case DECREMENT_COUNTER:
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.dispatch({
+    type: INCREMENT_COUNTER
+})
+store.dispatch({
+    type: DECREMENT_COUNTER
+})
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
